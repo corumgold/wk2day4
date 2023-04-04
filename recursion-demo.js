8f161c1b458507d9d97df016d6929f4f059ad16d
Cory Gold <goldcorum@gmail.com>
2023-04-04 15:47:45 -0400

    Add demo starter code

---
+++ recursion-demo.js
@@ -0,0 +1,61 @@
+console.log('\x1Bc'); // clears the console each run
+
+
+/* ******************* recursion ******************* */
+console.log('--------- Recursion Demo Start ---------');
+
+// Base Case and Recursive Case
+console.log('\n--------- Base Case and Recursive Case---------\n');
+
+// 1c. Recursive Function with Base Case and Recursive Case
+function factorial(number){
+
+    // THE BASE CASE: Can be resolved
+   
+
+    // THE RECURSIVE CASE: Must call itself until it reaches the Base Case.
+
+
+}
+
+// Verify Your Function Works 
+
+
+// Direct vs Indirect Recursion
+console.log('\n--------- Direct vs Indirect Recursion ---------\n');
+
+// Solving for Even or Odd Number Using Direct Recursion
+function evenOrOdd(number){
+
+    //BASE CASE 1
+    
+
+    //BASE CASE 2
+   
+
+    //RECURSIVE CASE
+    
+}
+
+// Verify Your Function Works 
+
+
+//Solving for Even or Odd using Indirect Recursion: 
+function isEven(n){
+
+    //Base Case
+   
+  
+    //Recursive Case
+
+  }
+  
+  function isOdd(n){
+    //Base Case
+    
+    
+    //Recursive Case
+    
+  }
+
+// Verify Your Function Works 
